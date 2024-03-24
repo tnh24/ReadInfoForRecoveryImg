41a9bd22fb6e0e9167c8743bca2fcec1176fb756
tnh24 <johnfrp.4k@gmail.com>
2024-03-24 20:52:28 +0630

    upload

---
+++ ReadInfo/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ReadInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(498, 30);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(517, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Blue;
+            this.richTextBox1.Location = new System.Drawing.Point(13, 111);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(591, 316);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 433);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(592, 44);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 489);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Read Infomation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
+
---
+++ ReadInfo/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadInfo
+{
+    public partial class Form1 : Form
+    {
+        //private RichTextBox richTextBox1;
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        public static readonly string Temp = Path.Combine(Path.GetTempPath(), "L4AT_adv");
+
+        public string selectedFilePath;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Directory.Exists(Temp))
+                {
+                    Directory.Delete(Temp, true);
+                    //MessageBox.Show("Directory and its contents deleted successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            // Create an instance of OpenFileDialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // Set the file dialog properties
+            openFileDialog.Title = "Open File";
+            openFileDialog.Filter = "Bin files (*.bin)|*.bin|Img Files (*.img)|*.img|Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            // Show the file dialog
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Get the selected file name and display it in the TextBox
+                string selectedFilePath = openFileDialog.FileName;               
+                textBox1.Text = selectedFilePath;                              
+
+            }
+
+            backgroundWorker1.RunWorkerAsync();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            backgroundWorker2.RunWorkerAsync();
+            
+            
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string sourceFilePath = textBox1.Text;
+
+            if (string.IsNullOrWhiteSpace(sourceFilePath))
+            {
+                MessageBox.Show("Please select a file first.");
+                return;
+            }
+
+            if (!File.Exists(sourceFilePath))
+            {
+                MessageBox.Show("The selected file does not exist.");
+                return;
+            }
+
+            // Create the destination path in the temporary folder
+            string destinationPath = Path.Combine(Temp, "boot.img");
+
+            // Ensure the destination directory exists
+            if (!Directory.Exists(Temp))]
+            {
+                Directory.CreateDirectory(Temp);
+            }
+
+            // Copy the selected file to the temporary folder
+            try
+            {
+                File.Copy(sourceFilePath, destinationPath, true);           
+                //MessageBox.Show("File copied successfully.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while copying the file: {ex.Message}");
+            }
+            
+        }
+
+        private static void ExtractFile()
+        {
+            byte[] bootimg = Properties.Resources.bootimg;
+            if (!Directory.Exists(Temp)) Directory.CreateDirectory(Temp);
+            if (!File.Exists(Path.Combine(Temp, "bootimg.exe")))
+                File.WriteAllBytes(Path.Combine(Temp, "bootimg.exe"), bootimg);
+        }
+        public static string runInDirectory(string exe, string cmd)
+        {
+            string Temp = Path.GetDirectoryName(exe);
+            Process proc = new Process
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    FileName = exe,
+                    Arguments = cmd,
+                    WorkingDirectory = Temp,
+                    RedirectStandardOutput = true
+                }
+            };
+            proc.Start();
+            return proc.StandardOutput.ReadToEnd();
+        }
+
+        private static string PropReader(string filter, string prop)
+        {
+            string text = File.ReadAllText(prop);
+            string result = "";
+            using (StringReader r = new StringReader(text))
+            {
+                string line;
+                while (r.Peek() != -1)
+                {
+                    line = r.ReadLine();
+                    if (line.Contains(filter)) result = line.Substring(line.IndexOf("=") + 1);
+                }
+            }
+            return result;
+        }
+        public void LoadInfo()
+        {
+            string defaultProp = "";
+            ExtractFile();
+            if (File.Exists(Temp + "\\boot.img"))
+            {
+                runInDirectory(Temp + "\\bootimg.exe", "--unpack-bootimg");
+                if (Directory.Exists(Temp + "\\initrd"))
+                {
+                    if (File.Exists(Temp + "\\initrd\\prop.default")) defaultProp = Temp + "\\initrd\\prop.default";
+                    if (File.Exists(Temp + "\\initrd\\default.prop")) defaultProp = Temp + "\\initrd\\default.prop";
+                    if (!string.IsNullOrEmpty(defaultProp))
+                    {
+                        // Clear the existing text in the RichTextBox
+                        ClearRichTextBox();
+
+                        using (StreamReader reader = new StreamReader(defaultProp))
+                        {
+                            string line;
+                            while ((line = reader.ReadLine()) != null)
+                            {                                
+                                if (line.Contains("ro.product.manufacturer"))
+                                {                                    
+                                    AppendToRichTextBox("Model Name\t=\t" + line.Substring(line.IndexOf("=") + 1));
+                                }
+
+                                if (line.Contains("ro.product.mod_device"))
+                                {
+                                    AppendToRichTextBox("Device Name\t=\t" + line.Substring(line.IndexOf("=") + 1));
+                                }
+                                if (line.Contains("ro.build.version.incremental"))
+                                {
+                                    AppendToRichTextBox("Miui Version\t=\t" + line.Substring(line.IndexOf("=") + 1));    
+                                }
+                                if (line.Contains("ro.build.version.release"))
+                                {
+                                    AppendToRichTextBox("Android Version\t=\t" + line.Substring(line.IndexOf("=") + 1));
+
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ClearRichTextBox()
+        {
+            if (richTextBox1.InvokeRequired)
+            {
+                richTextBox1.Invoke(new Action(() => richTextBox1.Clear()));
+            }
+            else
+            {
+                richTextBox1.Clear();
+            }
+        }
+
+        private void AppendToRichTextBox(string text)
+        {
+            if (richTextBox1.InvokeRequired)
+            {
+                richTextBox1.Invoke(new Action(() => richTextBox1.AppendText(text + Environment.NewLine)));
+            }
+            else
+            {
+                richTextBox1.AppendText(text + Environment.NewLine);
+            }
+        }
+
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            LoadInfo();
+
+        }
+    }
+}
